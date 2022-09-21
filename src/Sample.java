2e646cd877424de88c76f4493f82c1720bd66545
Mohsin121243 <mchoudhary4645@bths.edu>
2022-09-20 22:58:30 -0400

    Added favorite food

--- src/Sample.java
+++ src/Sample.java
@@ -2,7 +2,7 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Mohsin Choudhary");
         System.out.println("My favorite movie is The Godfather");
-        System.out.println("My favorite food is pasta");
+        System.out.println("My favorite food is Pasta");
 
     }
 }
